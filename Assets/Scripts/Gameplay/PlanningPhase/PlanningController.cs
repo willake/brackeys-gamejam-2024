3853e710254ce65b28ee872e8ed6b866fea47411
Hui En <huienlin.game@gmail.com>
2024-02-13 00:56:31 +0100

    update: implement a simple presentation of plan

--- Assets/Scripts/Gameplay/PlanningPhase/PlanningController.cs
+++ Assets/Scripts/Gameplay/PlanningPhase/PlanningController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Codice.CM.WorkspaceServer.DataStore;
 using Cysharp.Threading.Tasks;
 using Game.RuntimeStates;
 using Game.UI;
@@ -15,6 +16,8 @@ namespace Game.Gameplay
         [Header("References")]
         public PlanRuntimeState planRuntimeState;
         public PlanningPanel planningPanel;
+        public GameObject[] destinations;
+        public LineRenderer[] paths;
 
         private PlanState _planningState;
         private int _plannedSteps = 0;
@@ -36,6 +39,21 @@ namespace Game.Gameplay
                 .ObserveOnMainThread()
                 .Subscribe(actionType => AddActionPlan(actionType, Vector2.zero))
                 .AddTo(this);
+
+            planRuntimeState.Value
+                .ObserveCountChanged()
+                .ObserveOnMainThread()
+                .Subscribe(_ => PresentPlan(planRuntimeState.Value.ToArray()))
+                .AddTo(this);
+
+            foreach (var destination in destinations)
+            {
+                destination.gameObject.SetActive(false);
+            }
+            foreach (var path in paths)
+            {
+                path.gameObject.SetActive(false);
+            }
         }
 
         private void Update()
@@ -43,8 +61,8 @@ namespace Game.Gameplay
             if (canPlan && Input.GetKeyDown(KeyCode.Mouse0))
             {
                 Vector3 mousePos = Input.mousePosition;
-                Debug.Log($"mouse click at {mousePos}");
                 Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+                Debug.Log($"mouse click at {mousePos}, with world pos {mouseWorldPos}");
                 HandleClick(mousePos, mouseWorldPos);
             }
         }
@@ -114,6 +132,35 @@ namespace Game.Gameplay
             else
             {
                 ChangeState(PlanState.PlanMove);
+            }
+        }
+
+        public void PresentPlan(PlanNode[] planNodes)
+        {
+            Debug.Log("Update plan");
+            int movePlanIdx = 0;
+            int attackPlanIdx = 0;
+            Vector2 lastDestination = Vector2.zero;
+            for (int i = 0; i < planNodes.Length; i++)
+            {
+                PlanNodeType nodeType = planNodes[i].nodeType;
+                Vector3 destination = planNodes[i].destination;
+
+                if (nodeType == PlanNodeType.Move)
+                {
+                    destinations[movePlanIdx].SetActive(true);
+                    destinations[movePlanIdx].transform.position = destination;
+
+                    if (movePlanIdx > 0)
+                    {
+                        var line = paths[movePlanIdx - 1];
+                        line.gameObject.SetActive(true);
+                        line.SetPositions(new Vector3[] { lastDestination, destination });
+                    }
+
+                    lastDestination = destination;
+                    movePlanIdx += 1;
+                }
             }
         }
 
