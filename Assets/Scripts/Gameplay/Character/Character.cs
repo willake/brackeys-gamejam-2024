1ec36b1bd53aaeba88c791c02a3b474e3c8ef229
Hui En <huienlin.game@gmail.com>
2024-02-14 14:34:48 +0100

    update: write scripts for performing plans

--- Assets/Scripts/Gameplay/Character/Character.cs
+++ Assets/Scripts/Gameplay/Character/Character.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UniRx;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Game.Gameplay
 {
@@ -10,6 +13,10 @@ namespace Game.Gameplay
         [Header("Settings")]
         public float speed;
         public bool isDead = false;
+        public bool isMoving = false;
+        private Vector2 _destination = Vector2.zero;
+
+        private UnityEvent _onArriveDestination = new();
 
         private CharacterAnimator GetCharacterAnimator()
         {
@@ -22,6 +29,15 @@ namespace Game.Gameplay
         {
             Vector2 direction = (destination - new Vector2(transform.position.x, transform.position.y));
             GetCharacterAnimator().SetMoveSpeed(direction.x, direction.y, speed);
+            isMoving = true;
+            _destination = destination;
+        }
+
+        public async UniTask MoveToAsync(Vector2 destination)
+        {
+            MoveTo(destination);
+
+            await _onArriveDestination.AsObservable().Take(1);
         }
 
         public void Attack(Vector2 direction)
@@ -29,10 +45,34 @@ namespace Game.Gameplay
             GetCharacterAnimator().TriggerAttack();
         }
 
+        public async UniTask AttackAsync(Vector2 direction)
+        {
+            Attack(direction);
+
+            await GetCharacterAnimator().attackEndedEvent.AsObservable().Take(1);
+        }
+
         public void Die()
         {
             GetCharacterAnimator().TriggerDead();
             isDead = true;
+        }
+
+        private void Update()
+        {
+            if (isMoving)
+            {
+                float step = speed * Time.deltaTime;
+                Vector2.MoveTowards(transform.position, _destination, step);
+
+                float distanceToDestination = Vector2.Distance(transform.position, _destination);
+
+                if (distanceToDestination < 0.01f)
+                {
+                    isMoving = false;
+                    _onArriveDestination.Invoke();
+                }
+            }
         }
     }
 }
